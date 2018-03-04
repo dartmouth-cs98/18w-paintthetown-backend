8ba5e05db5d66e2ce3167fd7f5c6d69699c84114
mauesrog <Mauricio.Esquivel.Rogel.18@Dartmouth.edu>
2018-03-04 03:20:35 -0500

    added all master tokens and user data

--- app/config/index.js
+++ app/config/index.js
@@ -8,6 +8,11 @@ export default {
   facebookAppSecret: process.env.FACEBOOK_APP_SECRET,
   adminTokens: [
     process.env.ADMIN_TOKEN_MAU,
+    process.env.ADMIN_TOKEN_ACACIA,
+    process.env.ADMIN_TOKEN_MORGAN,
+    process.env.ADMIN_TOKEN_TED,
+    process.env.ADMIN_TOKEN_ALEX,
+    process.env.ADMIN_TOKEN_WYATT,
   ],
   adminData: [{
     name: process.env.FRIST_NAME_MAU,
@@ -15,5 +20,35 @@ export default {
     lastName: process.env.LAST_NAME_MAU,
     email: process.env.EMAIL_MAU,
     password: process.env.PASSWORD_MAU,
+  }, {
+    name: process.env.FRIST_NAME_ACACIA,
+    middleName: process.env.MIDDLE_NAME_ACACIA,
+    lastName: process.env.LAST_NAME_ACACIA,
+    email: process.env.EMAIL_ACACIA,
+    password: process.env.PASSWORD_ACACIA,
+  }, {
+    name: process.env.FRIST_NAME_MORGAN,
+    middleName: process.env.MIDDLE_NAME_MORGAN,
+    lastName: process.env.LAST_NAME_MORGAN,
+    email: process.env.EMAIL_MORGAN,
+    password: process.env.PASSWORD_MORGAN,
+  }, {
+    name: process.env.FRIST_NAME_TED,
+    middleName: process.env.MIDDLE_NAME_TED,
+    lastName: process.env.LAST_NAME_TED,
+    email: process.env.EMAIL_TED,
+    password: process.env.PASSWORD_TED,
+  }, {
+    name: process.env.FRIST_NAME_ALEX,
+    middleName: process.env.MIDDLE_NAME_ALEX,
+    lastName: process.env.LAST_NAME_ALEX,
+    email: process.env.EMAIL_ALEX,
+    password: process.env.PASSWORD_ALEX,
+  }, {
+    name: process.env.FRIST_NAME_WYATT,
+    middleName: process.env.MIDDLE_NAME_WYATT,
+    lastName: process.env.LAST_NAME_WYATT,
+    email: process.env.EMAIL_WYATT,
+    password: process.env.PASSWORD_WYATT,
   }],
 };
